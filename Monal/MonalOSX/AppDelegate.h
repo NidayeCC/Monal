feca4d252b755efd12c6cadbe839ffe2597c07c4
Anurodh Pokharel <anurodhp@alum.bu.edu>
2015-02-07 08:06:39 -0500

    added target

---
+++ Monal/MonalOSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MonalOSX
+//
+//  Created by Anurodh Pokharel on 2/7/15.
+//  Copyright (c) 2015 Monal.im. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
